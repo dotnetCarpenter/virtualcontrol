53028fbd85ac2f3794c1dee059057973e5f9cf39
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-10-15 03:13:28 -0200

    got the basics down while fixing svg.topath.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,15 @@
 import SVG from "svg.js"
 import Tile from "./tile.js"
 
-const canvas = SVG("canvas")
-
 function delay(f, time, ...args) {
 	return setTimeout(f, time, ...args)
 }
 
 function main() {
+	const canvas = SVG("canvas")
+
 	SVG.on(window, 'resize', () => { canvas.spof() })
+
 	canvas.on("click", (event) => {
 		const tile = new Tile(canvas, event.pageX + "," + event.pageY)
 		delay(tile.drawTile.bind(tile), 1000)
